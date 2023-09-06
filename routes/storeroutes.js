e932bff5311d89223a3bcec91ba201acb552931c
slbuisnesstree <143389218+slbuisnesstree@users.noreply.github.com>
2023-09-06 14:46:59 +0530

    Update storeroutes.js

--- routes/storeroutes.js
+++ routes/storeroutes.js
@@ -1,6 +1,6 @@
 const express =  require('express')
 
-const { getitems, getitem, createitem, deleteone, updateitem } = require('../controllers/controlstore')
+const { getitems, getitem, createitem, deleteone, updateitem, getMine } = require('../controllers/controlstore')
 
 const requireAuth = require('../middleware/requireAuth')
 
@@ -24,6 +24,10 @@ routes.delete('/:id',
 
 routes.patch('/:id',
     updateitem
+)
+
+routes.get('/mine',
+    getMine
 )
 
 module.exports = routes
