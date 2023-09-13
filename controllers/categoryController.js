427d41ff74ef8e79fc5dccdc12fd65c9478ea9d7
slbuisnesstree <143389218+slbuisnesstree@users.noreply.github.com>
2023-09-13 09:25:58 +0530

    Add files via upload

---
+++ controllers/categoryController.js
@@ -0,0 +1,28 @@
+const categories = require('../models/categoryModels')
+
+const getCategories = async (req, res) => {
+
+    const categ = await categories.find({}).sort({ createdAt:-1 })
+    res.status(200).json(categ)
+
+}
+
+const createCategory = async (req, res) => {
+
+    const {category, bgImage} = req.body 
+
+    try {
+        const categ = await categories.create({category, bgImage})
+        res.status(200).json(categ)
+    } catch (error) {
+        res.status(401).json({error:error.message})
+        console.log(error)
+    }
+} 
+
+module.exports = {
+
+    getCategories,
+    createCategory
+    
+}
